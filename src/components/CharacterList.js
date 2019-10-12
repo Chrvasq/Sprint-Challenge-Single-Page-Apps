9648243dad5e87d5f8f218c2bb34fc9f80c84398
Chris Vasquez <chrvasq@outlook.com>
2019-10-11 19:27:22 -0700

    Add character cards

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,26 +2,48 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import SearchForm from "./SearchForm";
+import CharacterCard from "./CharacterCard";
+import Axios from "axios";
 
+const Container = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 const Nav = styled.nav`
   margin-top: 20px;
 `;
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
   }, []);
 
+  console.log("Data", characters);
+
   return (
-    <section className="character-list">
-      <SearchForm />
-      <h2>TODO: `array.map()` over your state here!</h2>
-      <Nav>
-        <Link to="/">Home</Link>
-      </Nav>
-    </section>
+    <Container>
+      {characters.map(character => {
+        return (
+          <CharacterCard
+            key={character.id}
+            gender={character.gender}
+            image={character.image}
+            name={character.name}
+            origin={character.origin.name}
+            species={character.species}
+            status={character.status}
+          />
+        );
+      })}
+    </Container>
   );
 }
